88e6de1be936d29db76c85c1123d631daf191dff
Philipp Böhm <philipp-boehm@live.de>
2013-08-12 13:44:29 +0200

    Initial Commit

---
+++ server.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require 'sinatra'
+
+set :server, 'thin'
+
+episode_file = File.join(File.dirname(__FILE__), "simpsons.txt")
+episodes = File.open(episode_file, "r").each_line.to_a
+
+get '/' do
+  return episodes.sample
+end
