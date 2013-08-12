fb7cd5fcbad041a03fa2701d78a4b1545d25a8ad
Philipp Böhm <philipp-boehm@live.de>
2013-08-12 18:33:23 +0200

    Added newline to the returned episode

--- server.rb
+++ server.rb
@@ -9,5 +9,5 @@ episode_file = File.join(File.dirname(__FILE__), "simpsons.txt")
 episodes = File.open(episode_file, "r").each_line.to_a.shuffle
 
 get '/' do
-  return episodes.sample.strip
+  return episodes.sample
 end
